127d21d06de531513c00fdcbf27655d8f89e4282
Nikita Levchuk <code@levch.uk>
2025-10-28 19:03:10 +0100

    test: add more tests and configure fixtures

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,3 +1,13 @@
+$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+
 require 'minitest/autorun'
 
-$LOAD_PATH.unshift File.expand_path('../lib', __dir__)
+require 'tax_calculator'
+
+def fixture_path(filepath)
+  File.expand_path(File.join('fixtures', filepath), __dir__)
+end
+
+def fixture_read(filepath)
+  File.open(fixture_path(filepath), 'r', &:read)
+end
--- test/test_tax_calculator.rb
+++ test/test_tax_calculator.rb
@@ -1,62 +1,59 @@
 require_relative 'test_helper'
 
-require 'tax_calculator'
-require 'tax_calculator/input/text'
-require 'tax_calculator/output/text'
-require 'tax_calculator/errors/empty_input_error'
-require 'tax_calculator/errors/invalid_input_error'
-
 describe TaxCalculator do
-  before do
-    @options = {
-      input: TaxCalculator::Input::Text.new,
-      output: TaxCalculator::Output::Text.new
-    }
+  let(:tax_calculator) do
+    TaxCalculator::Processor.new(
+      file_reader: TaxCalculator::FileReader.new,
+      parser: TaxCalculator::Parsers::TxtParser.new,
+      serializer: TaxCalculator::Serializers::TxtSerializer.new
+    )
   end
 
   describe 'when the input is empty' do
     it 'raises an error' do
-      _(-> { TaxCalculator.run('', **@options) }).must_raise(
-        TaxCalculator::Errors::EmptyInputError
-      )
+      _(-> { tax_calculator.process('') }).must_raise(TaxCalculator::InvalidFilepathError)
     end
   end
 
-  describe 'when any line in the input is invalid' do
-    before do
-      # Missing the shelf price from the 2nd line
-      @invalid_input = "2 book at 12.49\n1 music CD at\n1 chocolate bar at 0.85"
-    end
-
+  describe 'when the file does not exist or is a directory' do
+    # Missing the shelf price from the 2nd line
     it 'raises an error' do
-      _(-> { TaxCalculator.run(@invalid_input, **@options) }).must_raise(
-        TaxCalculator::Errors::InvalidInputError
-      )
+      not_existed = 'test/fixtures/inputs/not_existed.txt'
+      _(-> { tax_calculator.process(not_existed) }).must_raise(TaxCalculator::FileReaderError)
+
+      directory = 'test/fixtures/inputs/'
+      _(-> { tax_calculator.process(directory) }).must_raise(TaxCalculator::FileReaderError)
+    end
+  end
+
+  describe 'when the input is invalid' do
+    # Missing the shelf price from the 2nd line
+    it 'raises an error' do
+      filepath = 'test/fixtures/inputs/invalid.txt'
+      _(-> { tax_calculator.process(filepath) }).must_raise(TaxCalculator::InvalidInputError)
     end
   end
 
   describe 'when the input exists' do
     it 'returns correct output for input 1' do
-      input = "2 book at 12.49\n1 music CD at 14.99\n1 chocolate bar at 0.85"
-      output = "2 book: 24.98\n1 music CD: 16.49\n1 chocolate bar: 0.85\nSales Taxes: 1.50\nTotal: 42.32"
+      filepath = 'test/fixtures/inputs/1.txt'
+      output = fixture_read('outputs/1.txt')
 
-      _(TaxCalculator.run(input, **@options)).must_equal output
+      _(tax_calculator.process(filepath)).must_equal output
     end
 
     it 'returns correct output for input 2' do
-      input = "1 imported box of chocolates at 10.00\n1 imported bottle of perfume at 47.50"
-      output = "1 imported box of chocolates: 10.50\n1 imported bottle of perfume: 54.65\nSales Taxes: 7.65\nTotal: 65.15"
+      filepath = 'test/fixtures/inputs/2.txt'
+      output = fixture_read('outputs/2.txt')
 
-      _(TaxCalculator.run(input, **@options)).must_equal output
+      _(tax_calculator.process(filepath)).must_equal output
     end
 
     it 'returns correct output for input 3' do
-      input = "1 imported bottle of perfume at 27.99\n1 bottle of perfume at 18.99\n1 packet of headache pills at 9.75\n3 imported boxes of chocolates at 11.25"
-      output = "1 imported bottle of perfume: 32.19\n1 bottle of perfume: 20.89\n1 packet of headache pills: 9.75\n3 imported boxes of chocolates: 35.55\nSales Taxes: 6.70\nTotal: 98.38"
-      # Seems the Sales Tax amount is invalid =>
-      # "1 imported bottle of perfume: 32.19\n1 bottle of perfume: 20.89\n1 packet of headache pills: 9.75\n3 imported boxes of chocolates: 35.55\nSales Taxes: 7.90\nTotal: 98.38"
+      filepath = 'test/fixtures/inputs/3.txt'
+      output = fixture_read('outputs/3.txt')
 
-      _(TaxCalculator.run(input, **@options)).must_equal output
+      _(tax_calculator.process(filepath)).must_equal output
     end
   end
 end
